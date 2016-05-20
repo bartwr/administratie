1b3e1a8e01c0ad25b794c422d040d1544297794e
Bart <bart@tuxion.nl>
2016-05-20 18:05:48 +0200

    feat(invoice) Row price is now calculated automatically

--- imports/ui/invoicing/invoice/invoice.jsx
+++ imports/ui/invoicing/invoice/invoice.jsx
@@ -1,3 +1,4 @@
+import Radium from 'radium';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
@@ -58,69 +59,114 @@ class Invoice extends Component {
     this.forceUpdate();
   }
 
+  // printInvoice :: Event -> Action downloadPDF
+  printInvoice(e) {
+    e.preventDefault();
+    return xepOnline.Formatter.Format('invoice-to-print',{render:'download'});
+  }
+
+  // calculateRowPrice :: ? -> ?
+  calculateRowPrice() { this.refs.rowPrice.value = this.refs.numberOf.value * this.refs.basePrice.value }
+
+  // handleNumberOfChange :: Event -> ?
+  handleNumberOfChange(e) { this.calculateRowPrice() }
+
+  // handleBasePriceChange :: Event -> ?
+  handleBasePriceChange(e) { this.calculateRowPrice() }
+
   render() {
     return (
       <div style={styles.overlay}>
-        <div style={styles.invoice}>
+        <div style={styles.invoice} id="invoice-to-print">
+          <div style={{flex: 1}}>
 
-          <div style={Styles.flexRow}>
+            <div style={Styles.flexRow}>
 
-            <section ref="clientAddress" style={Styles.flexCol}>
-              <b>{this.props.invoice.client}</b>
+              <section ref="clientAddress" style={Styles.flexCol}>
+                <b>{this.props.invoice.client}</b><br />
+                T.n.v. Bart Roorda<br />
+                Koediefstraat 2<br />
+                2511 CG  Den Haag
+              </section>
+
+              <section ref="providerAddress" style={Styles.flexCol}>
+                
+                <label style={styles.label}>Factuurnummer</label>
+                {this.props.invoice.invoiceNumber}
+                <br />
+                
+                <label style={styles.label}>Factuurdatum</label>
+                {this.props.invoice.invoiceDate}
+                <br />
+                
+                <label style={styles.label}>Periode</label>
+                ..
+                <br />
+                
+                <label style={styles.label}>Betreft</label>
+                {this.props.invoice.title}
+                <br />
+
+              </section>
+            </div>
+
+            <section ref="invoiceRows" style={Object.assign({}, styles.flexRow, styles.invoiceRows)}>
+
+              <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow)}>
+                <label style={Object.assign({}, styles.label, styles.descriptionRow)}>Omschrijving</label>
+                <label style={Object.assign({}, styles.label, styles.priceRow)}>Bedrag</label>
+              </div>
+              {this.renderInvoiceRows()}
+              <form onSubmit={this.handleInvoiceRowSubmit.bind(this)} style={Object.assign({}, styles.form, styles.hideWhilePrinting)}>
+                <input type="text" ref="title" placeholder="title" style={Object.assign({}, FormStyles.input, styles.input)} />
+                <input ref="numberOf" type="number" ref="numberOf" placeholder="numberOf" onChange={this.handleNumberOfChange.bind(this)} style={Object.assign({}, FormStyles.input, styles.input)} />
+                <input ref="basePrice" type="number" step="0.01" ref="basePrice" placeholder="basePrice" onChange={this.handleBasePriceChange.bind(this)} style={Object.assign({}, FormStyles.input, styles.input)} />
+                <input ref="rowPrice" type="number" step="0.01" ref="rowPrice" placeholder="rowPrice" style={Object.assign({}, FormStyles.input, styles.input)} />
+                <button type="submit" style={Object.assign({}, FormStyles.input, styles.input)}>v</button>
+              </form>
+
+              <div style={styles.invoiceEndRows}>
+                <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow, {borderBottom: 'none'})}>
+                  <label style={styles.label}>Subtotaal</label>
+                  <div>&euro; {this.formatPrice(this.props.invoice.invoicePrice())}</div>
+                </div>
+                <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow, {borderBottom: 'solid #E8E8E8 2px'})}>
+                  <label style={styles.label}>Totaal btw 21%</label>
+                  <div>&euro; {this.formatPrice(this.props.invoice.invoicePrice() * 0.21)}</div>
+                </div>
+                <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow, {borderBottom: 'none'})}>
+                  <label style={styles.label}>Totaal incl. btw</label>
+                  <div>&euro; {this.formatPrice(this.props.invoice.invoicePrice() * 1.21)}</div>
+                </div>
+              </div>
+
             </section>
 
-            <section ref="providerAddress" style={Styles.flexCol}>
-              
-              <label style={styles.label}>Factuurnummer</label>
-              {this.props.invoice.invoiceNumber}
-              <br />
-              
-              <label style={styles.label}>Factuurdatum</label>
-              {this.props.invoice.invoiceDate}
-              <br />
-              
-              <label style={styles.label}>Periode</label>
-              ..
-              <br />
-              
-              <label style={styles.label}>Betreft</label>
-              {this.props.invoice.title}
-              <br />
-
+            <section ref="endText" style={Object.assign({}, styles.endText)}>
+              Gelieve het bedrag van <b>&euro; {this.formatPrice(this.props.invoice.invoicePrice() * 1.21)}</b> over te maken naar rekeningnummer <b>NL38 TRIO 0776 4815 41</b><br />
+              ten name van <b>Tuxion</b>, onder vermelding van factuurnummer <b>{this.props.invoice.invoiceNumber}.</b>
             </section>
+
           </div>
 
-          <section ref="invoiceRows" style={Object.assign({}, styles.flexRow, styles.invoiceRows)}>
-
-            <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow)}>
-              <label style={Object.assign({}, styles.label, styles.descriptionRow)}>Omschrijving</label>
-              <label style={Object.assign({}, styles.label, styles.priceRow)}>Bedrag</label>
+          <footer ref="footer" style={Object.assign({}, Styles.flexRow, styles.footer)}>
+            <div style={Styles.flexCol}>
+              <b>Tuxion</b><br />
+              +31 (0) 6 46 38 68 64
             </div>
-            {this.renderInvoiceRows()}
-            <form onSubmit={this.handleInvoiceRowSubmit.bind(this)} style={styles.form}>
-              <input type="text" ref="title" placeholder="title" style={Object.assign({}, FormStyles.input, styles.input)} />
-              <input type="number" ref="numberOf" placeholder="numberOf" style={Object.assign({}, FormStyles.input, styles.input)} />
-              <input type="number" step="0.01" ref="basePrice" placeholder="basePrice" style={Object.assign({}, FormStyles.input, styles.input)} />
-              <input type="number" step="0.01" ref="rowPrice" placeholder="rowPrice" style={Object.assign({}, FormStyles.input, styles.input)} />
-              <button type="submit" style={Object.assign({}, FormStyles.input, styles.input)}>v</button>
-            </form>
-
-            <div style={styles.invoiceEndRows}>
-              <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow, {borderBottom: 'none'})}>
-                <label style={styles.label}>Subtotaal</label>
-                <div>&euro; {this.formatPrice(this.props.invoice.amount)}</div>
-              </div>
-              <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow, {borderBottom: 'solid #E8E8E8 2px'})}>
-                <label style={styles.label}>Totaal btw 21%</label>
-                <div>&euro; {this.formatPrice(this.props.invoice.amount * 0.21)}</div>
-              </div>
-              <div style={Object.assign({}, Styles.flexRow, styles.invoiceRow, {borderBottom: 'none'})}>
-                <label style={styles.label}>Totaal incl. btw</label>
-                <div>&euro; {this.formatPrice(this.props.invoice.amount * 1.21)}</div>
-              </div>
+            <div style={Styles.flexCol}>
+              bart@tuxion.nl<br />
+              www.tuxion.nl
             </div>
-
-          </section>
+            <div style={Styles.flexCol}>
+              Koediefstraat 2<br />
+              2511 CG&nbsp;&nbsp;Den Haag
+            </div>
+            <div style={Styles.flexCol}>
+              BTW NL048828889B01<br />
+              KVK 24371485
+            </div>
+          </footer>
 
         </div>
       </div>
@@ -137,22 +183,36 @@ var styles = {
     width: '100%',
     height: '100%',
     background: 'rgba(0, 0, 0, 0.8)',
+    '@media print': {
+    }
   },
   invoice: {
     width: '100%',
+    position: 'relative',
     maxWidth: '900px',
-    height: 'calc(100% - 5%)',
+    height: 'calc(100vh - 5%)',
     margin: '5% auto 0 auto',
-    padding: '210px 80px 20px 80px',
-    background: '#fff',
+    overflow: 'auto',
+    padding: '210px 80px 56px 80px',/* 20px + 36px used by footer = 56px */
+    background: '#fff url(/Logo-tuxionzwart_puntblauw_slogan.png) 80px 56px no-repeat',
+    backgroundSize: '180px',
     fontFamily: 'Helvetica, Arial, sans-serif',
     fontSize: '14px',
-    lineHeight: '24px',
+    lineHeight: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    '@media print': {
+      height: '100vh',
+      margin: '0',
+      padding: '165px 40px 56px 40px',
+      background: '#fff url(/Logo-tuxionzwart_puntblauw_slogan.png) 40px 26px no-repeat',
+      backgroundSize: '180px',
+    }
   },
   label: {
     fontWeight: 'bold',
     display: 'inline-block',
-    width: '200px',
+    width: '160px',
   },
   invoiceRows: {
     marginTop: '47px',
@@ -171,9 +231,41 @@ var styles = {
     textAlign: 'right'
   },
   invoiceEndRows: {
-    width: '280px',
+    marginTop: '37px',
+    width: '50%',
     alignSelf: 'flex-end'
   },
+  endText: {
+    position: 'absolute',
+    right: '20px',
+    bottom: '100px',
+    left: '80px',
+    '@media print': {
+      right: '20px',
+      bottom: '80px',
+      left: '20px',
+    }
+  },
+  footer: {
+    display: 'flex',
+    fontSize: '10px',
+    lineHeight: '14px',
+    position: 'absolute',
+    right: '20px',
+    bottom: '20px',
+    left: '80px',
+    overflow: 'none',
+    '@media print': {
+      right: '20px',
+      bottom: '0',
+      left: '20px',
+    }
+  },
+  hideWhilePrinting: {
+    '@media print': {
+      display: 'none'
+    }
+  }
 }
 
-export default Invoice;
+export default Radium(Invoice);
